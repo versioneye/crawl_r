51f3a82b30e3df6219b4a477757ec5d1305319fc
reiz <robert.reiz.81@gmail.com>
2014-05-25 16:14:16 +0200

    Add tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,49 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec"
+end
+
+require 'ruby_crawl'
+require 'rspec/autorun'
+require 'mongoid'
+require 'database_cleaner'
+require 'rubygems'
+require 'bundler'
+require 'factory_girl'
+
+require 'vcr'
+require 'webmock/rspec'
+require 'fakeweb'
+
+require 'versioneye-core'
+
+Mongoid.load!("config/mongoid.yml", :test)
+Mongoid.logger.level = Logger::ERROR
+Moped.logger.level   = Logger::ERROR
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+    FakeWeb.clean_registry
+  end
+
+  #include FactoryGirl into test DSL
+  config.include FactoryGirl::Syntax::Methods
+
+  VCR.configure do |c|
+    c.cassette_library_dir = 'spec/fixtures/vcr_cassettes/'
+    c.ignore_localhost = true
+    c.hook_into :webmock # or :fakeweb
+    c.allow_http_connections_when_no_cassette = true
+  end
+
+end
