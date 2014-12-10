ee350381072531283900ff9a5f42a0fe43659115
reiz <robert.reiz.81@gmail.com>
2014-12-10 13:39:42 +0100

    Add log output to job_scheduler_prod.sh

--- job_scheduler_prod.sh
+++ job_scheduler_prod.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "going to start common_crawl_worker"
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:common_crawl_worker &'
 
+echo "going to start packagist_crawl_worker"
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
@@ -10,9 +12,12 @@
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
 
+echo "going to start satis_crawl_worker"
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:satis_crawl_worker &'
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:satis_crawl_worker &'
 
+echo "going to start npm_crawl_worker"
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:npm_crawl_worker &'
 
+echo "going to start scheduler_crawl_r_prod"
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:scheduler_crawl_r_prod'
