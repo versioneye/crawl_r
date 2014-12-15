6c51b5cd963da40573c92d2f311557e2e827ad65
reiz <robert.reiz.81@gmail.com>
2014-12-15 12:22:35 +0100

    Update job_npm_crawl.sh

--- job_npm_crawl.sh
+++ job_npm_crawl.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+/bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:npm_crawl_worker &'
+/bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:npm_crawl_worker &'
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:npm_crawl_worker'
