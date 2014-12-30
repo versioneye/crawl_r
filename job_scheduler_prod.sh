5a656da17475509397c36d753802e301ae76f42d
reiz <robert.reiz.81@gmail.com>
2014-12-30 21:38:21 +0100

    Cleanup

--- job_scheduler_prod.sh
+++ job_scheduler_prod.sh
@@ -3,6 +3,13 @@
 echo "going to start common_crawl_worker"
 /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:common_crawl_worker &'
 
+echo "going to start satis_crawl_worker"
+/bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:satis_crawl_worker &'
+/bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:satis_crawl_worker &'
+
+echo "going to start scheduler_crawl_r_prod"
+/bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:scheduler_crawl_r_prod'
+
 # echo "going to start packagist_crawl_worker"
 # /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
 # /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
@@ -12,13 +19,6 @@ echo "going to start common_crawl_worker"
 # /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
 # /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:packagist_crawl_worker &'
 
-echo "going to start satis_crawl_worker"
-/bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:satis_crawl_worker &'
-/bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:satis_crawl_worker &'
-
 # echo "going to start npm_crawl_worker"
 # /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:npm_crawl_worker &'
 # /bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:npm_crawl_worker &'
-
-echo "going to start scheduler_crawl_r_prod"
-/bin/bash -l -c 'cd /ruby_crawl; /usr/local/bin/bundle exec rake versioneye:scheduler_crawl_r_prod'
