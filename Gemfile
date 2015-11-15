70b4d6211d574f2384a0956b1ca3110597882456
reiz <robert.reiz.81@gmail.com>
2015-11-15 16:39:42 +0100

    Update gems

--- Gemfile
+++ Gemfile
@@ -3,10 +3,10 @@ source "http://rubygems.org"
 gem 'log4r'              , '~> 1.1.0'
 gem 'bundler'            , '~> 1.10.6'
 
-gem 'versioneye-core'    , :git => 'git@github.com:versioneye/versioneye-core.git', :tag => 'v7.32.7'
+gem 'versioneye-core'    , :git => 'git@github.com:versioneye/versioneye-core.git', :tag => 'v7.33.2'
 # gem 'versioneye-core'    , :path => "~/workspace/versioneye/versioneye-core"
 
-gem 'rufus-scheduler', '3.1.7'
+gem 'rufus-scheduler', '3.1.9'
 
 group :development do
   gem "shoulda"  , ">= 0"
@@ -16,11 +16,11 @@ end
 
 group :test do
   gem 'simplecov'       , '~> 0.10.0'
-  gem 'rspec'           , '~> 3.3.0'
+  gem 'rspec'           , '~> 3.4.0'
   gem 'database_cleaner', '~> 1.5.1'
   gem 'factory_girl'    , '~> 4.5.0'
   gem 'capybara'        , '~> 2.5.0'
   gem 'vcr'             , '~> 2.9.2',  :require => false
-  gem 'webmock'         , '~> 1.21.0', :require => false
+  gem 'webmock'         , '~> 1.22.3', :require => false
   gem 'fakeweb'         , '~> 1.3.0'
 end
--- lib/versioneye/crawlers/biicode_crawler.rb
+++ lib/versioneye/crawlers/biicode_crawler.rb
@@ -8,44 +8,44 @@ class BiicodeCrawler < Versioneye::Crawl
 
   def self.crawl
     Settings.instance.reload_from_db GlobalSetting.new
-    
+
     index = 'https://webapi.biicode.com/v1/misc/blocks'
     body = JSON.parse HTTParty.get( index ).response.body
     blocks = body['blocks']
 
-    blocks.each do |block| 
-      process block  
+    blocks.each do |block|
+      process block
     end
-    nil 
+    nil
   rescue => e
     logger.error e.message
     logger.error e.backtrace.join("\n")
   end
 
 
-  def self.process block_name 
+  def self.process block_name
     msg = "process #{block_name}"
-    self.logger.info msg 
-    p msg 
-    
+    self.logger.info msg
+    p msg
+
     block_url = "https://webapi.biicode.com/v1/blocks/#{block_name}"
     block     = JSON.parse HTTParty.get( block_url ).response.body
-    
+
     tags_url  = "https://webapi.biicode.com/v1/blocks/#{block_name}/tags"
     tags_json = JSON.parse HTTParty.get( block_url ).response.body
-    tags      = tags_json['tags'] 
+    tags      = tags_json['tags']
     tags      = ["CPP"] if tags.nil? || tags.empty?
-    
+
     product = init_product block_name
     product.description = block['description']
     product.version = block['version']
-    product.tags = tags 
-    product.save 
-    
+    product.tags = tags
+    product.save
+
     versions_url = "https://webapi.biicode.com/v1/misc/versions/#{block_name}"
     versions = JSON.parse HTTParty.get( versions_url ).response.body
-    versions.each do |version_obj| 
-      process_version product, version_obj, block 
+    versions.each do |version_obj|
+      process_version product, version_obj, block
     end
   rescue => e
     logger.error "#{block_name} - #{e.message}"
@@ -53,36 +53,36 @@ class BiicodeCrawler < Versioneye::Crawl
   end
 
 
-  def self.process_version product, version_obj, block 
-    key = version_obj.keys.first 
+  def self.process_version product, version_obj, block
+    key = version_obj.keys.first
     released_string = version_obj[key]
 
     verso = product.version_by_number key
     return nil if verso
 
-    create_new_verison product, key, released_string, block 
+    create_new_verison product, key, released_string, block
   end
 
 
-  def self.create_new_verison product, version_string, released_string, block 
+  def self.create_new_verison product, version_string, released_string, block
     version_db                 = Version.new({version: version_string})
     version_db.released_string = released_string
     version_db.released_at     = date_from released_string
     version_db.status = fetch_status block, released_string
     version_db.tag    = fetch_tag block, released_string
-    
+
     product.versions.push version_db
     product.reindex = true
     product.save
 
     msg = " -- Biicode package: #{product.name} -- with new version: #{version_string}"
-    self.logger.info msg 
-    p msg 
+    self.logger.info msg
+    p msg
 
     CrawlerUtils.create_newest product, version_string, logger
     CrawlerUtils.create_notifications product, version_string, logger
 
-    create_links product, version_string, released_string, block 
+    create_links product, version_string, released_string, block
     create_dependencies product, version_string
     create_license product, version_string, released_string, block
   end
@@ -95,7 +95,7 @@ class BiicodeCrawler < Versioneye::Crawl
   end
 
 
-  def self.create_links product, version_string, released_string, block 
+  def self.create_links product, version_string, released_string, block
     scm_url = fetch_url block, released_string
     Versionlink.create_versionlink Product::A_LANGUAGE_BIICODE, product.prod_key, version_string, scm_url, "SCM"
 
@@ -107,14 +107,14 @@ class BiicodeCrawler < Versioneye::Crawl
   def self.create_dependencies product, version_string
     dep_url = "https://webapi.biicode.com/v1/blocks/#{product.name}/#{version_string}/requirements"
     requirements = JSON.parse HTTParty.get( dep_url ).response.body
-    return nil if requirements.nil? || requirements.empty? 
+    return nil if requirements.nil? || requirements.empty?
 
-    requirements.each do |requirement| 
+    requirements.each do |requirement|
       prod_key = "#{requirement['owner']}/#{requirement['creator']}/#{requirement['block_name']}/#{requirement['branch']}"
-      req_vers = requirement['time'] 
-      
+      req_vers = requirement['time']
+
       dep = Dependency.find_by( Product::A_LANGUAGE_BIICODE, product.prod_key, version_string, prod_key, req_vers, prod_key.downcase )
-      next if dep 
+      next if dep
 
       dependency = Dependency.new({:name => prod_key, :version => req_vers,
         :dep_prod_key => prod_key.downcase, :prod_key => product.prod_key,
@@ -123,7 +123,7 @@ class BiicodeCrawler < Versioneye::Crawl
       dependency.save
       dependency.update_known
     end
-  rescue => e 
+  rescue => e
     logger.error e.message
     logger.error e.backtrace.join("\n")
   end
@@ -131,83 +131,83 @@ class BiicodeCrawler < Versioneye::Crawl
 
   def self.fetch_status block, released_string
     return nil if block.nil? || block.empty?
-    return nil if block['deltas'].to_a.empty? 
+    return nil if block['deltas'].to_a.empty?
 
-    block['deltas'].each do |verso| 
+    block['deltas'].each do |verso|
       return verso['tag'] if verso['datetime'].eql?(released_string)
     end
-    nil 
-  rescue => e 
+    nil
+  rescue => e
     logger.error e.message
-    nil 
+    nil
   end
 
 
   def self.fetch_tag block, released_string
     return nil if block.nil? || block.empty?
-    return nil if block['deltas'].to_a.empty? 
+    return nil if block['deltas'].to_a.empty?
 
-    block['deltas'].each do |verso| 
-      return verso['versiontag'] if verso['datetime'].eql?(released_string) && !verso['versiontag'].to_s.empty? 
+    block['deltas'].each do |verso|
+      return verso['versiontag'] if verso['datetime'].eql?(released_string) && !verso['versiontag'].to_s.empty?
     end
-    nil 
-  rescue => e 
+    nil
+  rescue => e
     logger.error e.message
-    nil 
+    nil
   end
 
 
   def self.fetch_url block, released_string
     return nil if block.nil? || block.empty?
-    return nil if block['deltas'].to_a.empty? 
+    return nil if block['deltas'].to_a.empty?
 
-    block['deltas'].each do |verso| 
-      next if verso['origin'].nil? || verso['origin'].empty? 
-      
+    block['deltas'].each do |verso|
+      next if verso['origin'].nil? || verso['origin'].empty?
+
       return verso['origin']['url'] if verso['datetime'].eql?(released_string)
     end
-    nil 
-  rescue => e 
+    nil
+  rescue => e
     logger.error e.message
-    nil 
+    nil
   end
 
 
   def self.fetch_gh_repo_name block, released_string
     return nil if block.nil? || block.empty?
-    return nil if block['deltas'].to_a.empty? 
+    return nil if block['deltas'].to_a.empty?
 
-    block['deltas'].each do |verso| 
+    block['deltas'].each do |verso|
       next if verso['origin'].nil? || verso['origin'].empty?
-      next if verso['origin']['service'].casecmp('Github') != 0 
-      
+      next if verso['origin']['service'].casecmp('Github') != 0
+
       if verso['datetime'].eql?(released_string)
-        return "#{verso['origin']['username']}/#{verso['origin']['reponame']}" 
+        return "#{verso['origin']['username']}/#{verso['origin']['reponame']}"
       end
     end
-    nil 
-  rescue => e 
+    nil
+  rescue => e
     logger.error e.message
-    nil 
+    nil
   end
 
 
   def self.fetch_gh_branch block, released_string
     return nil if block.nil? || block.empty?
-    return nil if block['deltas'].to_a.empty? 
+    return nil if block['deltas'].to_a.empty?
 
-    block['deltas'].each do |verso| 
+    block['deltas'].each do |verso|
       next if verso['origin'].nil? || verso['origin'].empty?
-      next if verso['origin']['service'].casecmp('Github') != 0 
-      
+      next if verso['origin']['service'].casecmp('Github') != 0
+
       if verso['datetime'].eql?(released_string)
         return verso['origin']['branch']
       end
     end
-    nil 
-  rescue => e 
+    nil
+  rescue => e
     logger.error e.message
-    nil 
+    nil
   end
 
 
@@ -216,20 +216,20 @@ class BiicodeCrawler < Versioneye::Crawl
     return product if product
 
     Product.new({
-      :prod_type => Project::A_TYPE_BIICODE, 
-      :language => Product::A_LANGUAGE_BIICODE, 
-      :prod_key => name.downcase, 
-      :name => name, 
-      :name_downcase => name.downcase, 
+      :prod_type => Project::A_TYPE_BIICODE,
+      :language => Product::A_LANGUAGE_BIICODE,
+      :prod_key => name.downcase,
+      :name => name,
+      :name_downcase => name.downcase,
       :reindex => true})
   end
 
 
   def self.date_from released_string
     DateTime.parse(released_string)
-  rescue => e 
+  rescue => e
     logger.error e.message
-    nil 
+    nil
   end
 
 
