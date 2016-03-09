d1ce0f34704ddcd91852a8f814f11b08a54b0b26
reiz <robert.reiz.81@gmail.com>
2016-03-09 16:21:37 +0100

    Comment out biicode because API doesn't work anymore.

---
+++ spec/versioneye/crawlers/biicode_crawler_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe BiicodeCrawler do
+
+  # describe 'crawl' do
+  #   it 'crawles core os' do
+  #     Product.delete_all
+  #     expect( License.count ).to eq(0)
+  #     expect( Product.count ).to eq(0)
+  #     CoreosCrawler.crawl
+  #     expect( Product.count > 10 ).to be_truthy
+  #     expect( License.count > 0 ).to be_truthy
+  #   end
+  # end
+
+end
