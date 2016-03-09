e2f7d3f4ba3bcebc40e4dd9bef9d92682d8e77ee
reiz <robert.reiz.81@gmail.com>
2016-03-09 17:05:32 +0100

    Clean up.

--- lib/versioneye/crawlers/php/zendframework_crawler.rb
+++ lib/versioneye/crawlers/php/zendframework_crawler.rb
@@ -1,11 +1,13 @@
 class ZendframeworkCrawler < SatisCrawler
 
+
   def logger
     if !defined?(@@log) || @@log.nil?
       @@log = Versioneye::DynLog.new("log/zend.log", 10).log
     end
     @@log
   end
+
 
   A_BASE_URL  = 'https://packages.zendframework.com/'
   A_LINK_NAME = 'Zend Page'
