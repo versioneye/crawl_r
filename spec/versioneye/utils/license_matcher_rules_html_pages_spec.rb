a5dfd6ac17dc021cb6001c464210c32a6167ecdb
Timo Sulg <timgluz@gmail.com>
2017-01-26 13:31:27 +0100

    add specs for html pages with licenses;

---
+++ spec/versioneye/utils/license_matcher_rules_html_pages_spec.rb
@@ -0,0 +1,120 @@
+require 'spec_helper'
+require 'httparty'
+
+describe LicenseMatcher do
+  lm = LicenseMatcher.new
+
+  it "detects right licenses for foobar2000.org" do
+    VCR.use_cassette('rules/foobar2000') do
+      res = HTTParty.get('http://www.foobar2000.org/?page=License')
+      expect(res.code).to eq(200)
+
+      txt = lm.preprocess_text lm.clean_html lm.parse_html res.body
+      rls = lm.match_rules txt
+      expect(rls.size).to eq(3)
+      expect(rls[0][0]).to eq('apache-2.0')
+      expect(rls[1][0]).to eq('lgpl-2.1')
+      expect(rls[2][0]).to eq('zlib')
+    end
+  end
+
+  it "detects licenses from fontawesome page" do
+    VCR.use_cassette('rules/fontawesome') do
+      res = HTTParty.get('http://fontawesome.io/license/')
+      expect(res.code).to eq(200)
+
+      txt = lm.preprocess_text lm.clean_html lm.parse_html res.body
+      rls = lm.match_rules txt
+      expect(rls.size).to eq(3)
+      expect(rls[0][0]).to eq('mit')
+      expect(rls[1][0]).to eq('cc-by-3.0')
+      expect(rls[2][0]).to eq('ofl-1.1')
+    end
+  end
+
+  it "detects licenses from freeimage page" do
+    VCR.use_cassette('rules/freeimage') do
+      res = HTTParty.get('http://freeimage.sourceforge.net/license.html')
+      expect(res.code).to eq(200)
+
+      txt = lm.preprocess_text lm.clean_html lm.parse_html res.body
+      rls = lm.match_rules txt
+      expect(rls.size).to eq(3)
+      expect(rls[0][0]).to eq('gpl-2.0')
+      expect(rls[1][0]).to eq('freeimage')
+      expect(rls[2][0]).to eq('gpl-3.0')
+    end
+  end
+
+  it "detects license from Github raw text" do
+    VCR.use_cassette('rules/DarthFubuMVC') do
+      res = HTTParty.get 'https://raw.githubusercontent.com/DarthFubuMVC/fubumvc/master/license.txt'
+      expect(res.code).to eq(200)
+
+      rls = lm.match_rules lm.preprocess_text res.body
+      expect(rls.size).to eq(1)
+      expect(rls[0][0]).to eq('apache-2.0')
+    end
+  end
+
+  it "detects license from GongWPFDDragDrop readme" do
+    VCR.use_cassette('rules/GongWPFDragDrop') do
+      res = HTTParty.get 'https://github.com/punker76/gong-wpf-dragdrop'
+      expect(res.code).to eq(200)
+
+      txt = lm.preprocess_text lm.clean_html lm.parse_html res.body
+      rls = lm.match_rules txt
+      expect(rls.size).to eq(1)
+      expect(rls[0][0]).to eq('bsd-3')
+    end
+  end
+
+  it "detects license from GoogleTest wikipedia" do
+    VCR.use_cassette('rules/GoogleTest') do
+      res = HTTParty.get 'https://en.wikipedia.org/wiki/Google_Test'
+      expect(res.code).to eq(200)
+
+      txt = lm.preprocess_text lm.clean_html lm.parse_html res.body
+      rls = lm.match_rules txt
+      expect(rls.size).to eq(1)
+      expect(rls[0][0]).to eq('bsd-3')
+    end
+  end
+
+  it "detects license from Horde3d page" do
+    VCR.use_cassette('rules/Horde3d') do
+      res = HTTParty.get 'http://www.horde3d.org/license.html'
+      expect(res.code).to eq(200)
+
+      txt = lm.preprocess_text lm.clean_html lm.parse_html res.body
+      rls = lm.match_rules txt
+      expect(rls.size).to eq(1)
+      expect(rls[0][0]).to eq('epl-1.0')
+    end
+  end
+
+  it "detects license from HT4n page" do
+    VCR.use_cassette('rules/HT4n') do
+      res = HTTParty.get 'http://ht4n.softdev.ch/index.php/license'
+      expect(res.code).to eq(200)
+
+      txt = lm.preprocess_text lm.clean_html lm.parse_html res.body
+      rls = lm.match_rules txt
+      expect(rls.size).to eq(1)
+      expect(rls[0][0]).to eq('gpl-3.0')
+    end
+  end
+
+  it "detects license from libusb-win32 sourceforge page" do
+    VCR.use_cassette('rules/libusb-win32') do
+      res = HTTParty.get 'https://sourceforge.net/projects/libusb-win32/'
+      expect(res.code).to eq(200)
+
+      txt = lm.preprocess_text lm.clean_html lm.parse_html res.body
+      rls = lm.match_rules txt
+      expect(rls.size).to eq(2)
+      expect(rls[0][0]).to eq('gpl-3.0')
+      expect(rls[1][0]).to eq('lgpl-3.0')
+    end
+  end
+end
