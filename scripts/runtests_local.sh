50234eb45fbcfdb5f874fcbfcee24ec24ef08dc4
reiz <robert.reiz.81@gmail.com>
2017-07-06 07:59:54 +0200

    Update locale test runner

--- scripts/runtests_local.sh
+++ scripts/runtests_local.sh
@@ -4,7 +4,7 @@ echo "Going to run all specs"
 export RAILS_ENV="test"
 echo "Rails mode: $RAILS_ENV"
 
-rspec spec/versioneye/crawlers/hex_crawler_spec.rb
+rspec
 
 export RAILS_ENV="development"
 echo "Rails mode: $RAILS_ENV"
