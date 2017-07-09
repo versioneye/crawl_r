0705f73e8a72a9e29c64d0238bb545a3d118a141
reiz <robert.reiz.81@gmail.com>
2017-07-09 15:28:21 +0200

    Fix Tests

--- scripts/runtests_local.sh
+++ scripts/runtests_local.sh
@@ -4,7 +4,7 @@ echo "Going to run all specs"
 export RAILS_ENV="test"
 echo "Rails mode: $RAILS_ENV"
 
-rspec
+rspec --format documentation
 
 export RAILS_ENV="development"
 echo "Rails mode: $RAILS_ENV"
